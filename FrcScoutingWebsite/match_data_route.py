32429fa0c7585ba405dd449e36fdecb4de0c98eb
Fightfx1 <48966615+Fightfx1@users.noreply.github.com>
2020-03-01 00:21:22 +0200

    show games from matches

--- FrcScoutingWebsite/match_data_route.py
+++ FrcScoutingWebsite/match_data_route.py
@@ -7,9 +7,9 @@ def fix_columns_names(df):
     columns = df.columns.values
     columns = [
         ('Before Game',"Match Number"),('Before Game',"Team Number"),('Before Game',"color"),
-        ("Autonomous","Starting Power Cells"),('Autonomous',"A_Hole"),('Autonomous',"A_Hex"),('Autonomous',"A_Low"),
+        ("Autonomous","Starting Power Cells"),('Autonomous',"Hole"),('Autonomous',"Hex"),('Autonomous',"Low"),
         ('Autonomous',"Cross_Line"),
-        ("Teleop","Spin_Wheel"),("Teleop","Spin by color"),("Teleop","T_Hole"),("Teleop","T_Hex"),("Teleop","T_Low"),
+        ("Teleop","Spin_Wheel"),("Teleop","Spin by color"),("Teleop","Hole"),("Teleop","Hex"),("Teleop","Low"),
         ("End Game","Climb",),("End Game","Generator Switch Level"),("End Game","Park"),("End Game","Disconnect or Was broken"),("End Game","comments")
     ]
 
@@ -31,5 +31,11 @@ def match_data(match_numebr):
     df = df.loc[(df['Match Number'] == int(match_numebr)) & (~df['comments'].isin(['T']))]
     df['comments'] = df['comments'].replace(0,"")
 
-    fix_columns_names(df)
-    return render_template('MatchData.html',tables=[df.style.hide_index().render()])
+    df_red_teams = df.loc[df['color'] == "RED"]
+    df_blue_teams = df.loc[df['color'] == "BLUE"]
+
+
+    fix_columns_names(df_blue_teams)
+    fix_columns_names(df_red_teams)
+    
+    return render_template('MatchData.html',tables=[(df_red_teams.style.hide_index().render(), "Red teams"),(df_blue_teams.style.hide_index().render(),"Blue teams")])
